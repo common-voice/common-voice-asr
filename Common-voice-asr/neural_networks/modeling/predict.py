11c7f8034a500788ed6cf8980a2cdb7e8a732484
segerrity <ujy8jg@virginia.edu>
2025-05-29 10:59:32 -0400

    updating neural_networks syntax

--- Common-voice-asr/neural_networks/modeling/predict.py
+++ Common-voice-asr/neural_networks/modeling/predict.py
@@ -4,7 +4,7 @@ from loguru import logger
 from tqdm import tqdm
 import typer
 
-from Neural Networks.config import MODELS_DIR, PROCESSED_DATA_DIR
+from neural_networks.config import MODELS_DIR, PROCESSED_DATA_DIR
 
 app = typer.Typer()
 
