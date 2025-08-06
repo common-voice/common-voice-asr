c2053b3a5349b2a64bd8f91d12716bfb59040f17
moz-segerrity <jupyter@asr-workbench.c.moz-fx-dev-dfeller-cv.internal>
2025-08-06 01:33:07 +0000

    fixing different minor error in train

--- Common-voice-asr/neural_networks/modeling/train.py
+++ Common-voice-asr/neural_networks/modeling/train.py
@@ -393,7 +393,7 @@ def main(args):
 
     # --- Simplified Model Creation Logic ---
     num_classes = len(tokens)
-    model, collate_fn = model_creation(args.model_type, num_classes, args.hidden_dim, args.dropout, args.d_model, nhead=args.nhead,
+    model, collate_fn = model_creation(args.model_type, num_classes, args.hidden_dim, args.dropout, args.d_model, args.nhead,
                                        args.dim_feedforward, args.nlayers, args.lstm_hidden, args.lstm_layers, args.conv_layer)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
